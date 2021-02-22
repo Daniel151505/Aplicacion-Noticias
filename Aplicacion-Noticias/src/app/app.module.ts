9e49be916eca50d498cc76feecf515c4d4999ae7
Daniel151505 <daniel150520011@gmail.com>
2021-02-22 14:16:35 -0500

    Fix error

--- Aplicacion-Noticias/src/app/app.module.ts
+++ Aplicacion-Noticias/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
-import { ListadoNoticiasComponent } from './compoentes/listado-noticias/listado-noticias.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
 
