bd34bec15823ef3ac7eb114c7d782efbcec760d7
Daniel151505 <daniel150520011@gmail.com>
2021-02-22 16:35:19 -0500

    Retornando

--- Aplicacion-Noticias/src/app/services/noticia.service.ts
+++ Aplicacion-Noticias/src/app/services/noticia.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,10 @@ export class NoticiaService {
 
   constructor( private http: HttpClient) { }
 
-  getNoticia(parametros : any){
+  getNoticia(parametros : any): Observable<any>{
     const URL='https://newsapi.org/v2/top-headlines?country='
               + parametros.pais + '&category=' + parametros.categoria + '&apiKey=c561ce42a1e646fbaef209b6bc254519'
-  }
+    return this.http.get(URL)
+ }
 
 }
