cfba4b4c38119ec9239aeea71f9f3520107eddff
Daniel151505 <daniel150520011@gmail.com>
2021-02-22 15:56:14 -0500

    Agregando Metodo Buscar Noticias

--- Aplicacion-Noticias/src/app/app.component.ts
+++ Aplicacion-Noticias/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from './services/noticia.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Aplicacion-Noticias';
+
+  constructor(private noticiaService: NoticiaService) {
+
+  }
+  
+
+  buscarNoticias(parametros:any){
+    this.noticiaService.getNoticia(parametros).subscribe(data => {
+      
+    })
+  }
+
 }
