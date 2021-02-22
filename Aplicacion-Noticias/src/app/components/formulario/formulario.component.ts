8bd1f850032f3f9d0ffb60c90bad71f77545d76d
Daniel151505 <daniel150520011@gmail.com>
2021-02-22 15:07:38 -0500

    Agregando Arrays

--- Aplicacion-Noticias/src/app/components/formulario/formulario.component.ts
+++ Aplicacion-Noticias/src/app/components/formulario/formulario.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  categorias: any[] = [
+    { value: 'general', nombre: 'General'},
+    { value: 'business', nombre: 'Negocios'},
+    { value: 'entertainment', nombre: 'Entretenimiento'},
+    { value: 'health', nombre: 'Salud'},
+    { value: 'science', nombre: 'Ciencia'},
+    { value: 'sports', nombre: 'Deportes'},
+    { value: 'technology', nombre: 'Tecnología'},
+]
+
+paises: any[] = [
+  { value: 'ar', nombre: 'Argentina'},
+  { value: 'br', nombre: 'Brasil'},
+  { value: 'fr', nombre: 'Francia'},
+  { value: 'hu', nombre: 'Hungria'},
+  { value: 'mx', nombre: 'Mexico'},
+  { value: 'gb', nombre: 'Reino Unido'},
+]
   constructor() { }
 
   ngOnInit(): void {
